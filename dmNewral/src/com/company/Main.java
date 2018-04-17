ec9530ba30c3d09bb10ce4e940e2e1737e9e4c87
_iDong <dd_dong.bk@hotmail.com>
2018-04-18 00:27:08 +0700

    no

--- dmNewral/src/com/company/Main.java
+++ dmNewral/src/com/company/Main.java
@@ -7,22 +7,22 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        int data = 10;
+        int data = 260;
         int nodeInLay = 19;
         int nodeHidLay = 11;
         int nodeOutLay = 2;
-        double lernWeg = -0.4;
+        double lernWeg = 0.3;
 
         double[][] error = new double[data][nodeOutLay];
 
         double[] biasHid = new double[nodeHidLay];
         for (int i = 0; i < nodeHidLay; i++) {
-            biasHid[i] = -1+Math.random()*2;
+            biasHid[i] = Math.random();
         }
 
         double[] biasOut = new double[nodeOutLay];
         for (int i = 0; i < nodeOutLay; i++) {
-            biasOut[i] = -1+Math.random()*2;
+            biasOut[i] = Math.random();
         }
         double[][] inputIH = new double[data][nodeInLay];
         double[][] output = new double[data][nodeOutLay];
@@ -59,14 +59,14 @@ public class Main {
         double[][] IHWeight = new double[nodeInLay][nodeHidLay];
         for (int i = 0; i < nodeInLay; i++) {
             for (int j = 0; j < nodeHidLay; j++) {
-                IHWeight[i][j] = -1+Math.random()*2;
+                IHWeight[i][j] = Math.random();
             }
         }
 
         double[][] HOWeight = new double[nodeHidLay][nodeOutLay];
         for (int i = 0; i < nodeHidLay; i++) {
             for (int j = 0; j < nodeOutLay; j++) {
-                HOWeight[i][j] = -1+Math.random()*2;
+                HOWeight[i][j] = Math.random();
             }
         }
 
@@ -76,91 +76,201 @@ public class Main {
 
         double[] fdivH = new double[nodeHidLay];
         double[] graDianH = new double[nodeHidLay];
+        double[][] inputHO;
+        double[][] out = new double[nodeOutLay][data];
 
-        for (int f = 0; f < 1; f++) {
+        for (int f = 0; f < 50; f++) {
+            boolean st = false;
             for (int i = 0; i < data; i++) {
-                System.out.println("Round: " + (i + 1));
-                System.out.println("Weight(in-hid) = ");
-                printMatrix(IHWeight);
-                System.out.println("Bias(in-hid) = ");
-                printArray(biasHid);
+//                System.out.println("Round: " + (i + 1));
+//                System.out.println("Weight(in-hid) = ");
+//                printMatrix(IHWeight);
+//                System.out.println("Bias(in-hid) = ");
+//                printArray(biasHid);
 
-                double[][] inputHO = multiplyMat(inputIH, IHWeight);
+                inputHO = multiplyMat(inputIH, IHWeight);
                 inputHO = addmat(inputHO, biasHid);
                 inputHO = sigmoid(inputHO);
 
-            System.out.println("Input(hid-out)= ");
-            printMatrix(inputHO);
+
+//            System.out.println("Input(hid-out)= ");
+//            printMatrix(inputHO);
 //                System.out.println("Weight(hid-out) = ");
 //                printMatrix(HOWeight);
 //                System.out.println("Bias(hid-out) = ");
 //                printArray(biasOut);
-                double[][] out = multiplyMat(inputHO, HOWeight);
+                out = multiplyMat(inputHO, HOWeight);
                 out = addmat(out, biasOut);
                 out = sigmoid(out);
 
-                System.out.println("Output = ");
-                printMatrix(out);
-                System.out.println();
+//                System.out.println("Output = ");
+//                printMatrix(out);
+//                System.out.println();
+
+                //Back prop. begin
 
                 for (int j = 0; j < nodeOutLay; j++) {
-                    err[j] = out[i][j] - output[i][j];
-                    fdivO[j] = out[i][j] * (1 - out[i][j]);
-                    error[i][j] = err[j];
+                    err[j] = output[i][j] - out[i][j];
+//                    System.out.println("error["+j+"] = "+err[j]);
                 }
-//                System.out.println("error1 = " + err[0]);
-//                System.out.println("error2 = " + err[1]);
-//                if(Math.abs(err[0]) < 0.01 && Math.abs(err[1]) < 0.01)break;
+                if(Math.abs(err[0]) < 0.001 && Math.abs(err[1]) < 0.001){
+                    st = true;
+                    break;
+                }
+
                 for (int j = 0; j < nodeOutLay; j++) {
-                    graDianO[j] = err[j] * fdivO[j];
+//                    System.out.println("y = "+ out[i][j]);
+                    fdivO[j] = out[i][j] * (1 - out[i][j]);
+//                    System.out.println("fdiv = "+ fdivO[j]);
                 }
-//
-//            System.out.println("gra1O = "+graDianO[0]);
-//            System.out.println("gra2O = "+graDianO[1]);
+
+                for (int j = 0; j < nodeOutLay; j++) {
+                    graDianO[j] = err[j]*fdivO[j];
+//                    System.out.println("gradian["+j+"] ="+graDianO[j]);
+                }
+//use for hiden node
+                double[] sumGW = new double[nodeHidLay];
+                for (int j = 0; j < sumGW.length; j++) {
+                    sumGW[j] = 0;
+                }
+
                 for (int j = 0; j < nodeHidLay; j++) {
                     for (int k = 0; k < nodeOutLay; k++) {
-                        HOWeight[j][k] = (lernWeg * graDianO[k] * inputHO[i][k]) + HOWeight[j][k];
+                        sumGW[j] += graDianO[k]*HOWeight[j][k];
                     }
                 }
-
-                double sumGra = 0;
-                for (int j = 0; j < nodeOutLay; j++) {
-                    sumGra += (graDianO[j] * biasOut[j]);
+//end use for hiden node
+                for (int j = 0; j < nodeHidLay; j++) {
+                    for (int k = 0; k < nodeOutLay; k++) {
+//                        System.out.println("HOweight["+j+"]["+k+"] = "+HOWeight[j][k]);
+                        HOWeight[j][k] = (lernWeg*graDianO[k]*inputHO[i][j])+HOWeight[j][k];
+//                        System.out.println("HOweight["+j+"]["+k+"] NEW= "+HOWeight[j][k]);
+//                        System.out.println("inputHO["+i+"]["+j+"] = "+inputHO[i][j]);
+                    }
                 }
-
                 for (int j = 0; j < nodeOutLay; j++) {
                     biasOut[j] = (lernWeg * graDianO[j]) + biasOut[j];
                 }
-//                System.out.println("Weight(hid-out)ugrade = ");
-//                printMatrix(HOWeight);
-//                System.out.println("Bias(hid-out) upgrade= ");
-//                printArray(biasOut);
-
+//hidden node begin.
                 for (int j = 0; j < nodeHidLay; j++) {
-                    fdivH[j] = inputHO[i][j] * (1 - inputHO[i][j]);
-                    graDianH[j] = fdivH[j] * sumGra;
+                    fdivH[j] = inputHO[i][j]*(1-inputHO[i][j]);
                 }
 
-//                 System.out.println("sumGra = " + sumGra);
-//            System.out.println("gra1H = "+graDianH[0]);
-//            System.out.println("gra2H = "+graDianH[1]);
+                for (int j = 0; j < nodeHidLay; j++) {
+                    graDianH[j] = fdivH[j]*sumGW[j];
+                }
 
                 for (int j = 0; j < nodeInLay; j++) {
                     for (int k = 0; k < nodeHidLay; k++) {
-                        IHWeight[j][k] = (lernWeg * graDianH[k] * inputIH[i][k]) + IHWeight[j][k];
+//                        System.out.println("IHweight["+j+"]["+k+"] = "+IHWeight[j][k]);
+                        IHWeight[j][k] = (lernWeg*graDianH[k]*inputIH[i][j])+IHWeight[j][k];
+//                        System.out.println("IHweight["+j+"]["+k+"] NEW= "+IHWeight[j][k]);
+//                        System.out.println("inputIH["+i+"]["+j+"] = "+inputIH[i][j]);
                     }
                 }
                 for (int j = 0; j < nodeHidLay; j++) {
                     biasHid[j] = (lernWeg * graDianH[j]) + biasHid[j];
                 }
-//                System.out.println("Weight(in-hid)ugrade = ");
-//                printMatrix(IHWeight);
-//                System.out.println("Bias(in-hid) upgrade= ");
-//                printArray(biasHid);
-//                System.out.println();
+
+
+
+
+//re code start form here
+                //Back prop.
+//
+//                for (int j = 0; j < nodeOutLay; j++) {
+//                    err[j] = out[i][j] - output[i][j];
+//                    fdivO[j] = out[i][j] * (1 - out[i][j]);
+//                    error[i][j] = err[j];
+//                }
+//                System.out.println("error1 = " + err[0]);
+//                System.out.println("error2 = " + err[1]);
+//                System.out.println("fdiv1 = " + fdivO[0]);
+//                System.out.println("fdiv2 = " + fdivO[1]);
+////                if(Math.abs(err[0]) < 0.01 && Math.abs(err[1]) < 0.01)break;
+//                for (int j = 0; j < nodeOutLay; j++) {
+//                    graDianO[j] = err[j] * fdivO[j];
+//                }
+////
+//            System.out.println("gra1O = "+graDianO[0]);
+//            System.out.println("gra2O = "+graDianO[1]);
+//
+//
+//                double[] sumGra = new double[nodeHidLay];
+//                for (int j = 0; j < nodeHidLay; j++) {
+//                    sumGra[j] = 0;
+//                }
+//                for (int j = 0; j < nodeHidLay; j++) {
+////                    System.out.println();
+//                    for (int k = 0; k < nodeOutLay; k++) {
+//                        sumGra[j] += (graDianO[k] * HOWeight[j][k]);
+////                        System.out.println("graO="+ graDianO[k]);
+////                        System.out.println("HW weg="+ HOWeight[j][k]);
+//                    }
+////                    System.out.println("sum grad["+j+"]= "+ sumGra[j]);
+////                    System.out.println();
+//                }
+//
+//
+//                for (int j = 0; j < nodeHidLay; j++) {
+//                    for (int k = 0; k < nodeOutLay; k++) {
+////                        System.out.println();
+////                        System.out.println("HO weg = "+ HOWeight[j][k]);
+//
+//                        HOWeight[j][k] = (lernWeg * graDianO[k] * inputHO[i][k]) + HOWeight[j][k];
+//
+////                        System.out.println("lenweg = "+lernWeg);
+////                        System.out.println("grad = "+graDianO[k]);
+////                        System.out.println("input = "+ inputHO[i][k]);
+////                        System.out.println("newHO weg = "+ HOWeight[j][k]);
+////                        System.out.println();
+//                    }
+//                }
+//
+//
+//
+//                for (int j = 0; j < nodeOutLay; j++) {
+//                    biasOut[j] = (lernWeg * graDianO[j]) + biasOut[j];
+//                }
+////                System.out.println("Weight(hid-out)ugrade = ");
+////                printMatrix(HOWeight);
+////                System.out.println("Bias(hid-out) upgrade= ");
+////                printArray(biasOut);
+//
+//                for (int j = 0; j < nodeHidLay; j++) {
+//                    fdivH[j] = inputHO[i][j] * (1 - inputHO[i][j]);
+//                    graDianH[j] = fdivH[j] * sumGra[j];
+//                }
+//                System.out.println("gedianH = ");
+//                printArray(graDianH);
+////                 System.out.println("sumGra = " + sumGra);
+////            System.out.println("gra1H = "+graDianH[0]);
+////            System.out.println("gra2H = "+graDianH[1]);
+//
+//                for (int j = 0; j < nodeInLay; j++) {
+//                    for (int k = 0; k < nodeHidLay; k++) {
+//                        IHWeight[j][k] = (lernWeg * graDianH[k] * inputIH[i][k]) + IHWeight[j][k];
+//                    }
+//                }
+//                for (int j = 0; j < nodeHidLay; j++) {
+//                    biasHid[j] = (lernWeg * graDianH[j]) + biasHid[j];
+//                }
+////                System.out.println("Weight(in-hid)ugrade = ");
+////                printMatrix(IHWeight);
+////                System.out.println("Bias(in-hid) upgrade= ");
+////                printArray(biasHid);
+////                System.out.println();
+//end from here
             }
+            System.out.println("end ephoc"+(f+1));
+            System.out.println("Output = ");
+            printMatrix(out);
+            System.out.println();
+//            System.out.println("error = ");
+//            printMatrix(error);
+            if (st)break;
         }
-//        printMatrix(error);
+
     }
     private static double[][] sigmoid(double[][] f){
         int col = 0;
